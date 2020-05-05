e899a321050237a212cfe7b05735339b3bf9fa28
Diego <diego.vid.eco@gmail.com>
2020-05-05 11:16:01 -0500

    Prepara set

--- index.js
+++ index.js
@@ -1,47 +1,56 @@
-R = require('/home/diego/.nvm/versions/node/v10.16.3/lib/node_modules/ramda')
+R = require("/home/diego/.nvm/versions/node/v10.16.3/lib/node_modules/ramda"); // <3
 
-v = {}
-totalVoices = 0
+v = {};
+totalVoices = 0;
 
-msg.on('/canosc', (msg_) => {
-  var msg__ = R.fromPairs(R.splitEvery(2, msg_))
-  v["v"+msg__.voiceIndex] = msg__
-  //console.log("msg", msg__.scale)
-  //console.log("v", v)
-  totalVoices = msg__.canonSize
-  if(msg__.canonSize !== totalVoices) {
-    // console.log('will compile')
-    //main()
-  }
-})
+msg.on("/canosc", (msg_) => {
+  var msg__ = R.fromPairs(R.splitEvery(2, msg_));
+  v["v" + msg__.voiceIndex] = msg__;
+  // console.log("msg", msg__.r);
+  // console.log("v", v);
+  setAlgoDivData(msg__);
+});
 
-layer = R.invoker(1, 'diff')
-makeLayer = (key) =>
-solid(() => v[key].r, () => v[key].g, () => v[key].b)
-.mask(
-  shape(() =>v[key].shape ||  3).scale(() => v[key].scale || 1)
-  .modulate(noise(() => v[key].noise || 0))
-  // .repeat(() => v[key].repeat || 0)
-)
-.modulate(osc(() => v[key].r*20, () => v[key].g*3, () => v[key].b* 20))
-.scale(() => Math.sin(time*v[key].r*0.5 + v[key].g)* 4 )
-.scrollX(() => v[key].x || 0, () => v[key].xd || 0)
-.scrollY(() => v[key].y || 0, () => v[key].yd || 0)
-.saturate(0.7)
-// .color(0,2.4,5)
-.color(() => v[key].r*0.20, () => v[key].g*0.3, () => v[key].b* 1)
-main = () => R.reduce((acc, voice) =>
-layer(makeLayer(voice), acc), solid(0,0,0), Object.keys(v))
-main()
-// .scale(0.7, ()=> 0.7 + a.fft[3])
-.luma(2,1000, (6, ()=> 1 + a.fft[3]))
-.add(src(o0).scrollX(0.5), 0.4)
-.diff(o0)// o0
-.out()
+msg.on("/compile", ([voiceCount]) => {
+  var v1 = R.pipe( // update voiceCount
+    parseInt,
+    R.range(0),
+    R.map(n => 'v'+n),
+    R.pick(R.__, v)
+ )(voiceCount)
+  main(v1)
+  v = v1
+});
 
-s0.initScreen()
 
-src(o0).out()
+layer = R.invoker(1, "diff");
+makeLayer = (voice) =>
+  solid(() => v[voice].r || 0,() => v[voice].g || 0,() => v[voice].b || 0)
+    .mask(
+      shape(() => v[voice].shape || 3).scale(() => v[voice].scale || 1)
+      // .modulate(noise(() => v[voice].noise || 0))
+      // .repeat(() => v[voice].repeat || 0)
+    )
+    // .modulate(osc(() => v[voice].r*20, () => v[voice].g*3, () => v[voice].b* 20))
+    // .scale(() => Math.sin(v[voice].r * 5 + v[voice].g) * v[key].scale)
+    .scrollX(() => v[voice].x || 0,() => v[voice].xd || 0)
+    .scrollY(() => v[voice].y || 0,() => v[voice].yd || 0)
+    .saturate(0.7)
+    // .color(0,2.4,5)
+    // .color(() => v[voice].r * 0.2,() => v[voice].g * 0.3,() => v[voice].b * 1);
+main = (v) =>
+  R.reduce(
+    (acc, voice) => layer(makeLayer(voice), acc),
+    solid(0, 0, 0),
+    Object.keys(v)
+  )
+  // .scale(0.7, ()=> 0.7 + a.fft[3])
+  // .luma(2,1000, (6, ()=> 1 + a.fft[3]))
+  // .add(src(o0).scrollX(0.5), 0.4)
+  // .diff(o0)// o0
+  .out();
+main(v);
 
-osc(15, 0.01, 0.1).mult(osc(1, -0.1).modulate(osc(2).rotate(4,1), 20))
-    .out(o0)// o1
+s0.initScreen();
+
+src(o0).out();
